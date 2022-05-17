3737b96c87389493eed0a6d303e6d687a74ef885
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-05-17 09:31:28 -0300

    Fix applications menu

--- profiledef.sh
+++ profiledef.sh
@@ -29,4 +29,6 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/usr/share/applications/"]="0:0:755"
+  ["/usr/share/applications/browser.desktop"]="0:0:755"
 )
