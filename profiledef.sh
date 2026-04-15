fa41cd7669ac183663be0ef016ba056d37687439
joaquin (Pato) Decima <jdecima@vasak.net.ar>
2026-04-15 13:43:17 -0300

    fix: Corregir modos de arranque en profiledef.sh para mayor claridad

--- profiledef.sh
+++ profiledef.sh
@@ -8,7 +8,10 @@ iso_application="VasakOS Live"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+## GRUB
+#bootmodes=('bios.syslinux' 'uefi.grub')
+## systemd-boot
+bootmodes=('bios.syslinux' 'uefi.systemd-boot')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
